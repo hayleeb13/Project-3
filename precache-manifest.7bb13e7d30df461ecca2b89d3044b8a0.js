01c413c9d6dafc130fa1d64d41b2582ecc9d8552
hayleeb13 <hayleeb13@gmail.com>
2019-08-07 11:55:32 -0700

    Updates

---
+++ precache-manifest.7bb13e7d30df461ecca2b89d3044b8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84572c333e4877f58dfb8c41bfd925f5",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "d61f055949bcf866abf7",
+    "url": "/project-3/static/css/main.5362da7e.chunk.css"
+  },
+  {
+    "revision": "2f71b15344adfac7b6bc",
+    "url": "/project-3/static/js/2.70317bf9.chunk.js"
+  },
+  {
+    "revision": "d61f055949bcf866abf7",
+    "url": "/project-3/static/js/main.40638154.chunk.js"
+  },
+  {
+    "revision": "7763db6ab8e7687ed2e1",
+    "url": "/project-3/static/js/runtime~main.6a465fa7.js"
+  },
+  {
+    "revision": "4c7c4a13a4f1b0a5410ecb87f76560eb",
+    "url": "/project-3/static/media/happy-trees.4c7c4a13.jpg"
+  },
+  {
+    "revision": "1de01922c5ab5805f2b3cc5274d02756",
+    "url": "/project-3/static/media/heart-monitor-2.1de01922.jpg"
+  }
+]);
